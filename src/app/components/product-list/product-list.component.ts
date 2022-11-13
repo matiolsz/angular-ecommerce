d867ab8ffd4cc1149171c023ef9a7c966c1ff6f6
matiolsz <m.olszanka@be-tse.com>
2022-11-13 23:13:57 +0100

    - add searching product by name

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -12,6 +12,7 @@ export class ProductListComponent implements OnInit {
 
   products: Product[] = [];
   currentCategoryId!: number;
+  searchMode: boolean = false;
 
   constructor(private productService: ProductService, 
     private route: ActivatedRoute) { }
@@ -25,6 +26,15 @@ export class ProductListComponent implements OnInit {
 
   listProducts() {
 
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
+    if(this.searchMode){
+      this.handleSearchProducts();
+    }else{
+      this.handleListProducts();
+    }   
+  }
+
+  handleListProducts() {
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
     
     if(hasCategoryId) {
@@ -35,6 +45,17 @@ export class ProductListComponent implements OnInit {
     }
 
     this.productService.getProductList(this.currentCategoryId).subscribe(
+      data=> {
+        this.products = data;
+      }
+    )
+  }
+
+  handleSearchProducts() {
+
+    const theKeyword: string = this.route.snapshot.paramMap.get('keyword')!;
+
+    this.productService.searchProducts(theKeyword).subscribe(
       data=> {
         this.products = data;
       }
