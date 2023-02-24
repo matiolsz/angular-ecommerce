77df50990523af7e92eac919878c906f520cad02
matiolsz <m.olszanka@be-tse.com>
2023-02-24 12:44:35 +0100

    add validation for credit card in checkout compone

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -50,10 +50,10 @@ export class CheckoutComponent implements OnInit {
         zipCode: new FormControl('', [Validators.required, Validators.minLength(2), Luv2ShopValidators.notOnlyWhitespace])
       }),
       creditCard: this.formBuilder.group({
-        cardType: [''],
-        nameOnCard: [''],
-        cardNumber: [''],
-        securityCode: [''],
+        cardType: new FormControl('', [Validators.required]),
+        nameOnCard: new FormControl('', [Validators.required, Validators.minLength(2), Luv2ShopValidators.notOnlyWhitespace]),
+        cardNumber: new FormControl('', [Validators.required, Validators.pattern('[0-9]{16}')]),
+        securityCode: new FormControl('', [Validators.required, Validators.pattern('[0-9]{3}')]),
         expirationMonth: [''],
         expirationYear: ['']
       })
@@ -191,5 +191,24 @@ export class CheckoutComponent implements OnInit {
   get billingAddressCountry() {
     return this.checkoutFormGroup.get('billingAddress.country');
   }
+
+get cardType() {
+  return this.checkoutFormGroup.get('creditCard.cardType')
+}
+get nameOnCard() {
+  return this.checkoutFormGroup.get('creditCard.nameOnCard')
+}
+get cardNumber() {
+  return this.checkoutFormGroup.get('creditCard.cardNumber')
+}
+get securityCode() {
+  return this.checkoutFormGroup.get('creditCard.securityCode')
+}
+get expirationMonth() {
+  return this.checkoutFormGroup.get('creditCard.expirationMonth')
+}
+get expirationYear() {
+  return this.checkoutFormGroup.get('creditCard.expirationYear')
+}
 
 }
